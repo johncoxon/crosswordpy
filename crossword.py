99b9d10e3660166a771f3dbf5be1ca3361613459
John Coxon <john@johncoxon.co.uk>
2017-04-12 13:30:06 +0100

    Fixed a bug caused by the Guardian moving to https across all their webpages, and tidied up some code here and there for ease of comprehension.

--- crossword.py
+++ crossword.py
@@ -2,31 +2,34 @@
 # encoding: utf-8
 """
 A module which, on import, yields functions for automatically printing the Guardian crossword.
-Based on a Python script written by Jonny Nichols (University of Leicester) in the dim past.
+Based on a Python script written by Jonny Nichols (University of Leicester) in the distant past.
 
 Author:	John Coxon, Space Environment Physics group, University of Southampton
 Date:	2015/02/27
 """
-
-import datetime, inspect, os, urllib2, ConfigParser, crossword as cw
+import datetime as dt
+import os
+import urllib2
+import ConfigParser
+import inspect
+import crossword as cw
 
 # Note that there is code at the bottom of this file, after the functions are defined, which runs
 # upon the module being imported.
 
 #---------------------------------------------------------------------------------------------------
 
-def preferences(printerName, username, fitplot = True, ghostscript = True, pdfcrop = True,
+def preferences(printer_name, username, fitplot = True, ghostscript = True, pdfcrop = True,
 	PyPDF2 = True):
 	"""
 	Saves user preferences connected to dependencies, the name and features of the desired printer,
 	and the username of the user (for use in recording crossword progress).
 	"""
-
 	config = ConfigParser.SafeConfigParser()
 
 	# Set up the printer preferences.
 	config.add_section('Printer')
-	config.set('Printer', 'Name', printerName)
+	config.set('Printer', 'Name', printer_name)
 	config.set('Printer', 'fitplot', str(fitplot))
 
 	config.add_section('Dependencies')
@@ -45,7 +48,7 @@ def preferences(printerName, username, fitplot = True, ghostscript = True, pdfcr
 	cw.ghostscript = ghostscript
 	cw.pdfcrop = pdfcrop
 	cw.PyPDF2 = PyPDF2
-	cw.printer = printerName
+	cw.printer = printer_name
 	cw.username = username
 
 #---------------------------------------------------------------------------------------------------
@@ -55,29 +58,29 @@ def get_saturday_xword_no():
 	Gets the number of last Saturday's crossword by getting today's number and subtracting.
 	Takes account of situations in which Christmas Day will screw up the mathematics.
 	"""
-	
+
 	# Get the current date and day of week.
-	today = datetime.date.today()
+	today = dt.date.today()
 	weekday = today.weekday()
 
 	# Get the date on Saturday.
-	saturday = today - datetime.timedelta(weekday + 2)
+	saturday = today - dt.timedelta(weekday + 2)
 
 	# Get today's crossword number.
 	_, todayxwordno = get_xword_url()
 
-	# Get Christmas Day as a datetime object.
-	xmas = datetime.date(saturday.year, 12, 25)
+	# Get Christmas Day as a dt object.
+	xmas = dt.date(saturday.year, 12, 25)
 
-	# Subract to get Saturday's crossword number.
+	# Subtract to get Saturday's crossword number.
 	# If the two are on opposite sides of Christmas Day, subtract one less from today's xwordno.
 	if (today > xmas) & (saturday < xmas) & (xmas.weekday() != 6):
 		xwordno = todayxwordno - weekday
-	else:		
+	else:
 		xwordno = todayxwordno - (weekday + 1)
 
 	return xwordno
-	
+
 #---------------------------------------------------------------------------------------------------
 
 def next_xword_no(user):
@@ -91,17 +94,17 @@ def next_xword_no(user):
 	# Get the list of files from the archive directory and sort them.
 	files = os.listdir(os.path.dirname(inspect.getfile(inspect.currentframe())) + '/archive')
 	files.sort()
-	
+
 	try:
 		# Get the number of files and then select the last file in the list.
 		xwordno = files[-1]
 
 		# Get the start of the filename and add 1 to get the next crossword desired.
-		xwordno = int(xwordno[:5]) + 1		
+		xwordno = int(xwordno[:5]) + 1
 	except:
 		# If this does not work, 12395 is the first crossword for which there is a PDF.
 		xwordno = 12395
-		
+
 	return xwordno
 
 #---------------------------------------------------------------------------------------------------
@@ -122,7 +125,7 @@ def get_xword_url(xwordno = 0):
 			print('Error when accessing: ' + cw_url)
 			raise
 		cw_html = response.read()
-		cw_search = 'http://www.theguardian.com/crosswords/quick/'
+		cw_search = 'https://www.theguardian.com/crosswords/quick/'
 		cw_loc = cw_html.find(cw_search) + len(cw_search)
 		if cw_loc != -1:
 			xwordno = int(cw_html[cw_loc:cw_loc+5])
@@ -138,19 +141,19 @@ def get_xword_url(xwordno = 0):
 		raise
 
 	html = response.read()
-	
+
 	pdf_search = '>PDF'
 	pdf_loc = html.find(pdf_search)
 	if pdf_loc != -1:
 		pdf_url_str = html[pdf_loc-80:pdf_loc]
 	else:
 		raise StandardError('No PDF was found in the HTML of the page.')
-	
+
 	strsplit = pdf_url_str.split('"')
-	
+
 	foundURL = False
 
-	for substr in strsplit: 
+	for substr in strsplit:
 		if substr[0] == 'h':
 			pdfurl = substr
 			foundURL = True
@@ -176,7 +179,7 @@ def download_pdf(pdfurl, saturday = False, archive = False):
 		saturday: Set this to download the Saturday crossword.
 		archive: Set this to download an old crossword.
 	"""
-	
+
 	response = urllib2.urlopen(pdfurl)
 	if saturday != False:
 		filename = '/saturday.pdf'
@@ -184,15 +187,15 @@ def download_pdf(pdfurl, saturday = False, archive = False):
 		filename = '/archive/' + str(archive) + '.pdf'
 	else:
 		filename = '/today.pdf'
-		
+
 	pdffile = os.path.dirname(inspect.getfile(inspect.currentframe())) + filename
 	localFile = open(pdffile, 'w')
 	localFile.write(response.read())
-	
+
 	return pdffile
 
 #---------------------------------------------------------------------------------------------------
-	
+
 def crop_pdf(pdffile, pdfcrop = True, ghostscript = True):
 	"""
 	Crop the local PDF file to remove the large margins, using pdfcrop and ghostscript.
@@ -200,7 +203,7 @@ def crop_pdf(pdffile, pdfcrop = True, ghostscript = True):
 	INPUTS
 		pdffile: The PDF file to be cropped.
 	"""
-	
+
 	# Get just the path and basename, not the extension.
 	filename = os.path.splitext(pdffile)[0]
 
@@ -215,13 +218,13 @@ def crop_pdf(pdffile, pdfcrop = True, ghostscript = True):
 		os.popen('gs -o {0}-cropped.pdf {1} {0}-crop.pdf'.format(filename, gsParams))
 	else:
 		os.popen('cp {0}-crop.pdf {0}-cropped.pdf'.format(filename))
-	
+
 	# Return the new filename.
 	pdfcropped = '{0}-cropped.pdf'.format(filename)
-	
+
 	print pdfcropped
 	return pdfcropped
-	
+
 #---------------------------------------------------------------------------------------------------
 
 def rotate_pdf(pdffile):
@@ -260,7 +263,7 @@ def print_pdf(pdffile, landscape = False, fitplot = True):
 		landscape: Set this True to print the crossword in landscape (rotated 90 degrees).
 		fitplot: Set this to have the printer scale the file to the paper size.
 	"""
-	
+
 	printcmd = 'lp -n 2 '
 	if fitplot == True:
 		printcmd += '-o fitplot '
@@ -280,7 +283,7 @@ def delete_pdf(pdffile):
 	INPUTS
 		pdffile: The file to be deleted, alongside the associated -crop.pdf file.
 	"""
-	
+
 	os.popen('rm {0}'.format(pdffile))
 	os.popen('rm {0}-crop.pdf'.format(os.path.splitext(pdffile)[0]))
 	return 1
@@ -297,17 +300,17 @@ def today():
 	pdffile = download_pdf(pdfurl)
 	pdfcropped = crop_pdf(pdffile, pdfcrop = cw.pdfcrop, ghostscript = cw.ghostscript)
 
-	# Print it, delete the extraneous bits.	
+	# Print it, delete the extraneous bits.
 	printpdf = print_pdf(pdfcropped, fitplot = cw.fitplot)
 	deletepdf = delete_pdf(pdffile)
-	
+
 #---------------------------------------------------------------------------------------------------
 
 def saturday():
 	"""
 	Downloads and prints the (cropped) PDF of Saturday's Quick Crossword.
 	"""
-	
+
 	# Get the PDF, download it and crop it.
 	xwordno = get_saturday_xword_no()
 	pdfurl, _ = get_xword_url(xwordno = xwordno)
@@ -339,14 +342,14 @@ def archive(xwordno = 0):
 	pdffile = download_pdf(pdfurl, archive = actualxwordno)
 	pdfcropped = crop_pdf(pdffile, pdfcrop = cw.pdfcrop, ghostscript = cw.ghostscript)
 
-	# Work out whether the resulting file is landscape, set options accordingly.
-	# If we use ghostscript, it'll set the file rotation to 90 automatically, so the landscape
+	# Work out whether the resulting file is landscape, set options accordingly.
+	# If we use ghostscript, it'll set the file rotation to 90 automatically, so the landscape
 	# keyword can be left alone; if not, use a crude method to determine landscapeness.
 	if (cw.ghostscript == False) & ((int(xwordno) - 12394) % 6 == 0):
 		landscape = True
 	else:
 		landscape = False
-	
+
 	# Print it, delete the extraneous bits.
 	printpdf = print_pdf(pdfcropped, landscape = landscape, fitplot = cw.fitplot)
 	deletepdf = delete_pdf(pdffile)
@@ -361,12 +364,12 @@ read = config.read(os.path.dirname(__file__) + '/preferences.cfg')
 
 if read == []:
 	# Ask the user to set up the preferences.
-	printerName = raw_input('What is the name of the printer you will be using? ')
+	printer_name = raw_input('What is the name of the printer you will be using? ')
 	username = raw_input('What is your username on this computer? ')
-	cw.preferences(printerName, username)
+	cw.preferences(printer_name, username)
 else:
 
-	# Read the values back out of the config file.
+	# Read the values back out of the config file.
 	printer = config.get('Printer', 'Name')
 	username = config.get('Installation', 'Username')
 
